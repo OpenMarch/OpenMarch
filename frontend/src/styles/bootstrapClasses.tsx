3bc0d77d6a683ba58daa54461b23af8b843dabc2
Alex Dumouchelle <58530312+AlexDumo@users.noreply.github.com>
2023-09-26 12:55:28 -0400

    Style additions. Global bootstrap definitions

---
+++ frontend/src/styles/bootstrapClasses.tsx
@@ -0,0 +1,7 @@
+// This is a file that contains strings of pre-defined bootstrap classes.
+// This is done to avoid retyping the same class names over and over again.
+
+export const bsconfig = {
+    tableHeader: "row p-1 mx-auto bg-secondary text-light",
+    table: "table table-sm table-hover"
+}
