488aabfe69c857db22567f18cd526017fe770acd
Alex Dumouchelle <58530312+AlexDumo@users.noreply.github.com>
2023-10-16 17:33:53 -0400

    Canvas fits inside toolbars
    fix #4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,37 +9,44 @@ import Topbar from './components/toolbar/Topbar';
 import Sidebar from './components/toolbar/Sidebar';
 import { SelectedPageProvider } from './context/SelectedPageContext';
 import { SelectedMarcherProvider } from './context/SelectedMarcherContext';
+import PixiCanvas from './components/PixiCanvas';
 
 function App() {
   return (
     // Context for the selected page. Will change when more specialized
     <SelectedPageProvider>
       <SelectedMarcherProvider>
-        <div className="App">
-          <div className="toolbar-container">
-            <Topbar />
+        <div className="app-container">
+          {/* <div className="toolbar-container"> */}
+          <Topbar />
+          <div className="secondary-container">
             <Sidebar />
+            <Canvas />
+            {/* <PixiCanvas /> */}
           </div>
-          {/* <Canvas /> */}
-          {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-          <Toolbar />
         </div>
+        <div className="canvas-container">
+        </div>
+        {/* </div> */}
       </SelectedMarcherProvider>
     </SelectedPageProvider >
   );
 }
 
 export default App;
+
+// {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+// {/* <Toolbar /> */ }
