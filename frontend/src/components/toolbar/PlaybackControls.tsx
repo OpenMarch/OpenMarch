621841ab42d36d6013be8c7d6afca6317f1d6caa
Alex Dumouchelle <58530312+AlexDumo@users.noreply.github.com>
2023-11-10 17:46:15 -0500

    changes stashed for merge

--- frontend/src/components/toolbar/PlaybackControls.tsx
+++ frontend/src/components/toolbar/PlaybackControls.tsx
@@ -1,7 +1,55 @@
+import React, { MouseEventHandler, useCallback } from "react";
+import { ButtonGroup, Button } from "react-bootstrap";
+import { FaFastBackward, FaBackward, FaPause, FaPlay, FaForward, FaFastForward } from "react-icons/fa";
+import { useSelectedPage } from "../../context/SelectedPageContext";
+import { usePageStore } from "../../stores/Store";
+
 function PlaybackControls() {
+    const [playing, setPlaying] = React.useState(false);
+    const { selectedPage, setSelectedPage } = useSelectedPage()!;
+    const { pages } = usePageStore()!;
+
+    const changeSelectedPageHandler: MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
+        const newPageOrder = parseInt(e.currentTarget.value);
+        if (selectedPage) {
+            setSelectedPage(pages.find(page => page.order === newPageOrder) || selectedPage);
+        }
+    }, [selectedPage, pages, setSelectedPage]);
+
+    const lowestPageOrder = useCallback(() => {
+        if (selectedPage && pages.length > 0) {
+            const orders = Object.values(pages).map(page => page.order);
+            return Math.min(...orders);
+        }
+
+        // Default, may cause issues
+        return 0;
+    }, [selectedPage, pages]);
+
+    const togglePlay = () => { setPlaying(!playing) };
     return (
         <div className="playback-controls">
-            {/* toolbar buttons */}
+            <ButtonGroup aria-label="Basic example">
+                <Button variant="secondary" title="First page"
+                    value={selectedPage ? selectedPage.order - 1 : -1}
+                    onClick={changeSelectedPageHandler}
+                    disabled={selectedPage?.order === lowestPageOrder()}
+                >
+                    <FaFastBackward />
+                </Button>
+                <Button variant="secondary" title="Previous page">
+                    <FaBackward />
+                </Button>
+                <Button variant="secondary" onClick={togglePlay} title="Play or pause">
+                    {playing ? <FaPause /> : <FaPlay />}
+                </Button>
+                <Button variant="secondary" title="Next page">
+                    <FaForward />
+                </Button>
+                <Button variant="secondary" title="Last page">
+                    <FaFastForward />
+                </Button>
+            </ButtonGroup>
         </div>
     );
 }
--- frontend/src/components/toolbar/PlaybackToolbar.tsx
+++ frontend/src/components/toolbar/PlaybackToolbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { FaPlay, FaBackward, FaFastBackward, FaForward, FaFastForward, FaPause } from "react-icons/fa";
 
 const Toolbar: React.FC = () => {
@@ -18,19 +19,19 @@ const Toolbar: React.FC = () => {
                 </div>
             </div>
             <ButtonGroup aria-label="Basic example">
-                <Button variant="secondary">
+                <Button data-toggle="tooltip" data-placement="bottom" variant="secondary" title="First page">
                     <FaFastBackward />
                 </Button>
-                <Button variant="secondary">
+                <Button data-toggle="tooltip" data-placement="bottom" variant="secondary" title="Previous page">
                     <FaBackward />
                 </Button>
-                <Button variant="secondary" onClick={togglePlay}>
+                <Button data-toggle="tooltip" data-placement="bottom" variant="secondary" onClick={togglePlay} title="Play or pause">
                     {playing ? <FaPause /> : <FaPlay />}
                 </Button>
-                <Button variant="secondary">
+                <Button data-toggle="tooltip" data-placement="bottom" variant="secondary" title="Next page">
                     <FaForward />
                 </Button>
-                <Button variant="secondary">
+                <Button data-toggle="tooltip" data-placement="bottom" variant="secondary" title="Last page">
                     <FaFastForward />
                 </Button>
             </ButtonGroup>
