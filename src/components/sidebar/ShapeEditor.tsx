882827324787c0f9d9307da97001f7ac356ef7f4
dukcc <dukkcc@proton.me>
2024-12-06 16:44:17 -0800

    start changing shape editor ui

--- src/components/sidebar/ShapeEditor.tsx
+++ src/components/sidebar/ShapeEditor.tsx
@@ -4,6 +4,7 @@ import { useShapePageStore } from "@/stores/ShapePageStore";
 import { MarcherShape } from "@/global/classes/canvasObjects/MarcherShape";
 import { useCallback } from "react";
 import * as Form from "@radix-ui/react-form";
+import { Plus, Trash } from "@phosphor-icons/react";
 import {
     SelectItem,
     Select,
@@ -46,94 +47,111 @@ export default function ShapeEditor() {
         return (
             <Form.Root
                 id={`${marcherShape.shapePage.id}-shapeForm`}
-                className="flex flex-col gap-2"
+                className="flex flex-col gap-16"
             >
-                {marcherShape.canvasMarchers.length > 0 && (
-                    <div className="">
-                        {marcherShape.canvasMarchers.length} Marchers:{" "}
-                        <p className="max-h-64 overflow-y-auto text-sub">
-                            {marcherShape.canvasMarchers
-                                .map((cm) => cm.marcherObj.drill_number)
-                                .join(", ")}
-                        </p>
-                    </div>
-                )}
-                {marcherShape.shapePath.points.map(
-                    (point, index) =>
-                        index > 0 && ( // do not render the first shape (move)
-                            <Form.Field
-                                key={index}
-                                name="section"
-                                className="flex items-center justify-between"
-                            >
-                                <Form.Label className="text-body text-text/80">
-                                    Segment {index}
-                                </Form.Label>
-
-                                <Form.Control asChild>
-                                    <Select
-                                        required
-                                        value={
-                                            SvgCommands[point.command].command
-                                        }
-                                        onValueChange={(
-                                            newSvg: SvgCommandEnum,
-                                        ) =>
-                                            updateSegment({
-                                                shapePageId:
-                                                    marcherShape.shapePage.id,
-                                                index,
-                                                newSvg,
-                                            })
-                                        }
-                                    >
-                                        <SelectTriggerButton label={"Type"} />
-                                        <SelectContent>
-                                            {(index > 1
-                                                ? Object.values(SvgCommands)
-                                                : secondSegmentSvgCommands
-                                            ).map((cmd) => {
-                                                return (
-                                                    <SelectItem
-                                                        key={cmd.command}
-                                                        value={cmd.command}
-                                                    >
-                                                        {
-                                                            cmd.readableDescription
-                                                        }
-                                                    </SelectItem>
-                                                );
-                                            })}
-                                        </SelectContent>
-                                    </Select>
-                                </Form.Control>
-                                <Form.Message
-                                    match={"valueMissing"}
-                                    className="text-sub leading-none text-red"
+                <div className="flex flex-col gap-12">
+                    <h5 className="text-h5">Segments</h5>
+                    {marcherShape.shapePath.points.map(
+                        (point, index) =>
+                            index > 0 && ( // do not render the first shape (move)
+                                <Form.Field
+                                    key={index}
+                                    name="section"
+                                    className="flex items-center justify-between"
                                 >
-                                    Please enter a value.
-                                </Form.Message>
-                            </Form.Field>
-                        ),
-                )}
-                <Button
-                    onClick={() => {
-                        marcherShape.addSegment();
-                    }}
-                    type="button"
-                >
-                    Add Segment
-                </Button>
-                <Button
-                    onClick={() => {
-                        marcherShape.deleteSegment(
-                            marcherShape.shapePath.points.length - 1,
-                        );
-                    }}
-                    type="button"
-                >
-                    Delete Segment
-                </Button>
+                                    <Form.Label className="text-body text-text/80">
+                                        {index}
+                                    </Form.Label>
+
+                                    <Form.Control asChild>
+                                        <Select
+                                            required
+                                            value={
+                                                SvgCommands[point.command]
+                                                    .command
+                                            }
+                                            onValueChange={(
+                                                newSvg: SvgCommandEnum,
+                                            ) =>
+                                                updateSegment({
+                                                    shapePageId:
+                                                        marcherShape.shapePage
+                                                            .id,
+                                                    index,
+                                                    newSvg,
+                                                })
+                                            }
+                                        >
+                                            <SelectTriggerButton
+                                                label={"Type"}
+                                            />
+                                            <SelectContent>
+                                                {(index > 1
+                                                    ? Object.values(SvgCommands)
+                                                    : secondSegmentSvgCommands
+                                                ).map((cmd) => {
+                                                    return (
+                                                        <SelectItem
+                                                            key={cmd.command}
+                                                            value={cmd.command}
+                                                        >
+                                                            {
+                                                                cmd.readableDescription
+                                                            }
+                                                        </SelectItem>
+                                                    );
+                                                })}
+                                            </SelectContent>
+                                        </Select>
+                                    </Form.Control>
+                                    <Form.Message
+                                        match={"valueMissing"}
+                                        className="text-sub leading-none text-red"
+                                    >
+                                        Please enter a value.
+                                    </Form.Message>
+                                </Form.Field>
+                            ),
+                    )}
+                    <div className="flex flex-wrap gap-6">
+                        <Button
+                            onClick={() => {
+                                marcherShape.addSegment();
+                            }}
+                            type="button"
+                            size="compact"
+                            variant="secondary"
+                        >
+                            <Plus size={18} /> Segment
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                marcherShape.deleteSegment(
+                                    marcherShape.shapePath.points.length - 1,
+                                );
+                            }}
+                            type="button"
+                            size="compact"
+                            variant="red"
+                        >
+                            <Trash size={18} /> Segment
+                        </Button>
+                    </div>
+                </div>
+                <div className="flex-col gap-12">
+                    {marcherShape.canvasMarchers.length > 0 && (
+                        <>
+                            <h5 className="text-h5">
+                                {marcherShape.canvasMarchers.length} Marchers
+                            </h5>
+                            <p className="max-h-72 overflow-y-auto text-sub">
+                                {marcherShape.canvasMarchers
+                                    .map((cm) => cm.marcherObj.drill_number)
+                                    .join(", ")}
+                            </p>
+                        </>
+                    )}
+                </div>
             </Form.Root>
         );
     };
