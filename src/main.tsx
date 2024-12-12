b74437c2e490585c7b41b218453f7d98caf13769
Ben Godwin <bdgodwin@amazon.com>
2024-12-11 22:13:15 -0500

    Testing PR workflows

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "@fontsource/dm-mono";
 import "@fontsource/dm-sans";
 import { ThemeProvider } from "./context/ThemeContext";
 
+// test comment
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider>
