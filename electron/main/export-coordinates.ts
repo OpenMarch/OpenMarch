9d55c933d0edff7412486d02b38cfea2670b5c5e
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2024-12-22 09:22:40 -0500

    Remove puppeteer and trailing whitespace

--- electron/main/export-coordinates.ts
+++ electron/main/export-coordinates.ts
@@ -1,43 +1,44 @@
-import { dialog } from "electron";
-import * as fs from "fs";
-import puppeteer, { Browser } from "puppeteer";
+// import { dialog } from "electron";
+// import * as fs from "fs";
+// import puppeteer, { Browser } from "puppeteer";
 
-let browser: Browser;
+// let browser: Browser;
 
 export async function generatePDF(renderedPages: string[]) {
+    console.log("generatePDF");
     // Launch browser on app ready
-    browser = await puppeteer.launch();
+    // browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
+    // const page = await browser.newPage();
 
-    const combinedHtml = renderedPages.join(
-        '<div style="page-break-after: always"></div>',
-    );
+    // const combinedHtml = renderedPages.join(
+    //     '<div style="page-break-after: always"></div>',
+    // );
 
-    // Set page content
-    await page.setContent(combinedHtml);
+    // // Set page content
+    // await page.setContent(combinedHtml);
 
-    // Generate PDF
-    const pdfBuffer = await page.pdf({
-        printBackground: true,
-        format: "letter",
-        margin: {
-            top: "1cm",
-            right: "1cm",
-            bottom: "1cm",
-            left: "1cm",
-        },
-    });
+    // // Generate PDF
+    // const pdfBuffer = await page.pdf({
+    //     printBackground: true,
+    //     format: "letter",
+    //     margin: {
+    //         top: "1cm",
+    //         right: "1cm",
+    //         bottom: "1cm",
+    //         left: "1cm",
+    //     },
+    // });
 
-    // Show save dialog
-    const { filePath } = await dialog.showSaveDialog({
-        filters: [{ name: "PDF", extensions: ["pdf"] }],
-    });
+    // // Show save dialog
+    // const { filePath } = await dialog.showSaveDialog({
+    //     filters: [{ name: "PDF", extensions: ["pdf"] }],
+    // });
 
-    // Save PDF file
-    if (filePath) {
-        fs.writeFileSync(filePath, pdfBuffer);
-    }
+    // // Save PDF file
+    // if (filePath) {
+    //     fs.writeFileSync(filePath, pdfBuffer);
+    // }
 
-    await page.close();
+    // await page.close();
 }
