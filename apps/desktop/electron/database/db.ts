ef9152249edcb27f68eb4aac8d6ba783d2c28430
Jake Pusateri <jakepusateri@gmail.com>
2025-05-26 14:22:05 -0400

    fix tests

---
+++ apps/desktop/electron/database/db.ts
@@ -0,0 +1,9 @@
+import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
+import * as schema from "./migrations/schema";
+import { Database } from "better-sqlite3";
+
+export type DB = BetterSQLite3Database<typeof schema>;
+
+export function getOrm(db: Database): DB {
+    return drizzle({ client: db, schema, casing: "snake_case", logger: true });
+}
