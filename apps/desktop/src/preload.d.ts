82ae1b49ddd8cc4a459a4ca3de30239c75727a12
John Bostick <john.bostick@gatech.edu>
2025-05-31 17:40:35 -0400

    Add the main canvas to the global window object to allow for easy access throughout OpenMarch

--- apps/desktop/src/preload.d.ts
+++ apps/desktop/src/preload.d.ts
@@ -1,10 +1,12 @@
 import { ElectronApi, PluginsApi } from "../electron/preload/index";
+import { OpenMarchCanvas } from "./global/classes/canvasObjects/OpenMarchCanvas";
 
 declare global {
     // eslint-disable-next-line no-unused-vars
     interface Window {
         electron: ElectronApi;
         plugins: PluginsApi;
+        canvas: OpenMarchCanvas;
     }
 }
 
