75d7388dc1cf29f968cc83c53f2c4204abda4ad4
dukc <dukkcc@proton.me>
2025-06-02 21:49:03 -0700

    Cq
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    add tabs to toolbar

--- apps/desktop/src/components/toolbar/Toolbar.tsx
+++ apps/desktop/src/components/toolbar/Toolbar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Tabs, TabContent, TabItem, TabsList } from "@openmarch/ui";
 import { FileTab } from "./tabs/FileTab";
 import AlignmentTab from "./tabs/AlignmentTab";
@@ -25,18 +24,5 @@ export default function Topbar() {
                 <ViewTab />
             </TabContent>
         </Tabs>
-=======
-import PlaybackControls from "./sections/PlaybackControls";
-import UiSettingsToolbar from "./sections/UiSettingsToolbar";
-import AlignmentToolbar from "./sections/AlignmentToolbar";
-
-export default function Topbar() {
-    return (
-        <div className="flex w-full flex-wrap gap-8">
-            <PlaybackControls />
-            <UiSettingsToolbar />
-            <AlignmentToolbar />
-        </div>
->>>>>>> 1f8fe29 (ui: add new sidebar layout, fix icon imports)
     );
 }
