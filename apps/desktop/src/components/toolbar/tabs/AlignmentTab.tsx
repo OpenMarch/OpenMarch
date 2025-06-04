f3f2b702d61b36a7bbb0d9a23d4b05503470d728
dukc <dukkcc@proton.me>
2025-06-03 20:06:42 -0700

    move timeline controls, polishes

--- apps/desktop/src/components/toolbar/tabs/AlignmentTab.tsx
+++ apps/desktop/src/components/toolbar/tabs/AlignmentTab.tsx
@@ -8,89 +8,11 @@ import {
     AlignCenterVerticalSimpleIcon,
     DotsThreeOutlineIcon,
     DotsThreeOutlineVerticalIcon,
-    RewindIcon,
-    SkipBackIcon,
-    PlayIcon,
-    PauseIcon,
-    SkipForwardIcon,
-    FastForwardIcon,
 } from "@phosphor-icons/react";
 import RegisteredActionButton from "@/components/RegisteredActionButton";
 import ToolbarSection from "@/components/toolbar/ToolbarSection";
-import { useSelectedPage } from "@/context/SelectedPageContext";
-import { useIsPlaying } from "@/context/IsPlayingContext";
-import { topBarComponentProps } from "@/global/Interfaces";
 
 export default function AlignmentTab() {
-    return (
-        <div className="flex w-full flex-wrap gap-8">
-            <PlaybackControls />
-            <AlignmentToolbar />
-        </div>
-    );
-}
-
-function PlaybackControls({ className }: topBarComponentProps) {
-    const { selectedPage } = useSelectedPage()!;
-    const { isPlaying } = useIsPlaying()!;
-
-    return (
-        <ToolbarSection aria-label="Playback Controls">
-            <RegisteredActionButton
-                registeredAction={RegisteredActionsObjects.firstPage}
-                disabled={
-                    !selectedPage ||
-                    selectedPage.previousPageId === null ||
-                    isPlaying
-                }
-            >
-                <RewindIcon size={24} />
-            </RegisteredActionButton>
-
-            <RegisteredActionButton
-                registeredAction={RegisteredActionsObjects.previousPage}
-                disabled={
-                    !selectedPage ||
-                    selectedPage.previousPageId === null ||
-                    isPlaying
-                }
-            >
-                <SkipBackIcon size={24} />
-            </RegisteredActionButton>
-
-            <RegisteredActionButton
-                registeredAction={RegisteredActionsObjects.playPause}
-                disabled={!selectedPage || selectedPage.nextPageId === null}
-            >
-                {isPlaying ? <PauseIcon size={24} /> : <PlayIcon size={24} />}
-            </RegisteredActionButton>
-
-            <RegisteredActionButton
-                registeredAction={RegisteredActionsObjects.nextPage}
-                disabled={
-                    !selectedPage ||
-                    selectedPage.nextPageId === null ||
-                    isPlaying
-                }
-            >
-                <SkipForwardIcon size={24} />
-            </RegisteredActionButton>
-
-            <RegisteredActionButton
-                registeredAction={RegisteredActionsObjects.lastPage}
-                disabled={
-                    !selectedPage ||
-                    selectedPage.nextPageId === null ||
-                    isPlaying
-                }
-            >
-                <FastForwardIcon size={24} />
-            </RegisteredActionButton>
-        </ToolbarSection>
-    );
-}
-
-function AlignmentToolbar() {
     const { uiSettings } = useUiSettingsStore();
     return (
         <div className="flex w-full flex-wrap gap-8">
