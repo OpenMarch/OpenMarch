156d9e2448d3a651e147815dbd4a0591a3cf539f
dukc <dukkcc@proton.me>
2025-06-06 20:10:00 -0700

    fix sidebar modal scroll, polish components

--- packages/ui/src/components/base/Switch.tsx
+++ packages/ui/src/components/base/Switch.tsx
@@ -6,11 +6,11 @@ import { forwardRef } from "react";
 export const Switch = forwardRef<HTMLButtonElement, RadixSwitchProps>(
     ({ className, ...props }, ref) => {
         const rootClassname = clsx(
-            "bg-fg-2 relative h-[25px] w-[42px] cursor-pointer border-2 focus-visible:outline-none border-stroke rounded-full focus-visible:border-accent data-[state=checked]:bg-accent disabled:cursor-not-allowed disabled:opacity-50 duration-150 ease-out",
+            "bg-fg-2 relative h-[25px] w-[42px] cursor-pointer border focus-visible:outline-none border-stroke rounded-full focus-visible:border-accent bg-fg-2 disabled:cursor-not-allowed disabled:opacity-50 duration-150 ease-out",
             className,
         );
         const thumbClassname = clsx(
-            "translate-x-[1px] block size-[20px] rounded-full bg-white  transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[17.5px]",
+            "translate-x-[1px] block size-[20px] rounded-full bg-text/80 data-[state=checked]:bg-accent transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[17.5px]",
         );
         return (
             <RadixSwitch.Root ref={ref} className={rootClassname} {...props}>
