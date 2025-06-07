4107aea2952abe33373cbfdc22baea2880ae8ed1
dukc <dukkcc@proton.me>
2025-06-06 17:24:47 -0700

    move section styles to marchers modal

--- apps/desktop/src/components/sidebar/Sidebar.tsx
+++ apps/desktop/src/components/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import MusicModal from "@/components/music/MusicModal";
 import FieldModal from "@/components/field/FieldModal";
 import MarchersModal from "@/components/marcher/MarchersModal";
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import SectionAppearanceModal from "@/components/section/SectionAppearanceModal";
 <<<<<<< HEAD
@@ -9,6 +10,8 @@ import BugReport from "./BugReport";
 >>>>>>> 7d8b28a (rearrange, add tabs ui component)
 =======
 >>>>>>> 7dbba7f (move timeline controls, polishes)
+=======
+>>>>>>> dfd9976 (move section styles to marchers modal)
 import TipsAndTricks from "../guides/TipsAndTricks";
 import Plugins from "../plugins/Plugins";
 
@@ -20,6 +23,10 @@ export default function Sidebar() {
         >
             <div className="flex flex-col gap-16">
                 <MarchersModal />
+<<<<<<< HEAD
+=======
+                <PagesModal />
+>>>>>>> dfd9976 (move section styles to marchers modal)
                 <MusicModal />
                 <FieldModal />
             </div>
