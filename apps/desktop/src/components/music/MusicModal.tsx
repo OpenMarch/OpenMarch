8e0360c40d7f8947e2b85e41381d56199302f25d
dukc <dukkcc@proton.me>
2025-07-05 10:52:15 -0700

    move docs to link

--- apps/desktop/src/components/music/MusicModal.tsx
+++ apps/desktop/src/components/music/MusicModal.tsx
@@ -1,6 +1,6 @@
 import AudioSelector from "./AudioSelector";
 import { SidebarModalLauncher } from "../sidebar/SidebarModal";
-import { XIcon } from "@phosphor-icons/react";
+import { BooksIcon, XIcon } from "@phosphor-icons/react";
 import { useSidebarModalStore } from "@/stores/SidebarModalStore";
 import {
     getLastBeatOfTempoGroup,
@@ -38,7 +38,6 @@ function MusicModalContents() {
     const [newGroupFormHidden, setNewGroupFormHidden] = React.useState(
         measures.length > 0,
     );
-    const [helpBoxOpen, setHelpBoxOpen] = React.useState(false);
     const tempoGroups = useMemo(
         () => TempoGroupsFromMeasures(measures),
         [measures],
@@ -88,40 +87,22 @@ function MusicModalContents() {
                 </div>
                 <div className="flex items-center gap-8">
                     <h4 className="text-h4 leading-none">Tempo Groups</h4>
-                    <button
-                        className="text-text-subtitle hover:text-accent rounded-full duration-150 ease-out"
-                        onClick={() => setHelpBoxOpen(true)}
+                    <a
+                        href="https://github.com/OpenMarch/OpenMarch"
+                        target="_blank"
+                        rel="noreferrer"
                     >
-                        ?
-                    </button>
+                        <Button
+                            size={"compact"}
+                            variant="secondary"
+                            className="w-fit min-w-fit whitespace-nowrap"
+                        >
+                            <BooksIcon size={22} />
+                            See docs
+                        </Button>
+                    </a>
                 </div>
                 <div id="tempo-groups" className="mx-12 flex flex-col gap-8">
-                    <div
-                        className="bg-fg-2 rounded-4 border-stroke bg-bg-1 flex flex-col gap-8 border p-16"
-                        hidden={!helpBoxOpen}
-                    >
-                        <h5 className="font-medium">What is a tempo group?</h5>
-                        <ul className="text-text-subtitle ml-8 list-inside list-disc text-sm">
-                            <li>
-                                Tempo groups are collections of measures that
-                                have the same time signature and tempo
-                            </li>
-                            <li>
-                                Groups that have the same time signature and
-                                tempo are combined into a single group, unless
-                                the second group has a name. (I.e. &quot;A&quot;
-                                or &quot;Closer&quot;)
-                            </li>
-                        </ul>
-                        <div className="flex justify-end">
-                            <Button
-                                variant="secondary"
-                                onClick={() => setHelpBoxOpen(false)}
-                            >
-                                Close
-                            </Button>
-                        </div>
-                    </div>
                     {tempoGroups.map((tempoGroup, i) => (
                         <div key={i} className="flex flex-col gap-8">
                             <TempoGroupCard
