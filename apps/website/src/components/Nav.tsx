d21273fb1e7b5a603b1b3ed7269d54903f609ece
dukc <dukkcc@proton.me>
2025-07-07 21:18:47 -0700

    Redesign website nav bar, start new lander

--- apps/website/src/components/Nav.tsx
+++ apps/website/src/components/Nav.tsx
@@ -1,18 +1,27 @@
 import { Button } from "@openmarch/ui";
 import { LogoTextMark } from "./LogoTextMark.tsx";
 import React, { useState } from "react";
-import { LogoGitHub, LogoDiscord, LogoPatreon } from "./Logos.tsx";
+import {
+    ArrowLineDownIcon,
+    DiscordLogoIcon,
+    GithubLogoIcon,
+    ListIcon,
+    PatreonLogoIcon,
+    TShirtIcon,
+    XIcon,
+} from "@phosphor-icons/react";
+import { DownloadIcon } from "@phosphor-icons/react/dist/ssr";
 
 export default function Nav() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
-        <nav className="sticky top-0 z-[99] flex h-[4rem] w-full flex-col">
-            <nav className="border-stroke bg-modal flex w-full items-center justify-between border-b px-64 py-16 backdrop-blur-md max-[750px]:px-24">
+        <nav className="sticky top-16 z-[99] flex h-[3.5rem] w-full flex-col gap-8">
+            <nav className="border-stroke bg-modal relative flex w-full items-center justify-between rounded-full border px-32 py-12 backdrop-blur-md max-[750px]:px-24">
                 <a href="/">
                     <LogoTextMark />
                 </a>
-                <div className="flex items-center gap-16 max-[675px]:hidden">
+                <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center gap-16 max-[675px]:hidden">
                     <a
                         href="/"
                         className="text-body text-text hover:text-accent duration-150 ease-out"
@@ -37,61 +46,48 @@ export default function Nav() {
                     >
                         About
                     </a>
-                    <a href="https://github.com/OpenMarch/OpenMarch">
-                        <Button
-                            variant="secondary"
-                            size="compact"
-                            className="items-center gap-6"
-                        >
-                            <LogoGitHub />
-                            Star
-                        </Button>
-                    </a>
-                    <a href="https://discord.gg/eTsQ98uZzq">
-                        <Button
-                            variant="secondary"
-                            size="compact"
-                            className="items-center gap-6"
-                        >
-                            <LogoDiscord />
-                            Discord
-                        </Button>
-                    </a>
-                    <a href="https://www.patreon.com/openmarch">
-                        <Button
-                            variant="secondary"
-                            size="compact"
-                            className="items-center gap-6"
-                        >
-                            <LogoPatreon />
-                            Donate
-                        </Button>
-                    </a>
-                    <a href="https://store.openmarch.com">
-                        <Button variant="secondary" size="compact">
-                            Merch
-                        </Button>
-                    </a>
                     <a href="/download">
                         <Button variant="primary" size="compact">
                             Download
                         </Button>
                     </a>
                 </div>
+                <div className="flex items-center gap-16 max-[675px]:hidden">
+                    <a
+                        href="https://store.openmarch.com"
+                        className="text-text hover:text-accent duration-150"
+                    >
+                        <TShirtIcon size={24} />
+                    </a>
+                    <a
+                        href="https://www.patreon.com/openmarch"
+                        className="text-text hover:text-accent duration-150"
+                    >
+                        <PatreonLogoIcon size={24} />
+                    </a>
+                    <a
+                        href="https://discord.gg/eTsQ98uZzq"
+                        className="text-text hover:text-accent duration-150"
+                    >
+                        <DiscordLogoIcon size={24} />
+                    </a>
+                    <a
+                        href="https://github.com/OpenMarch/OpenMarch"
+                        className="text-text hover:text-accent duration-150"
+                    >
+                        <GithubLogoIcon size={24} />
+                    </a>
+                </div>
                 <button
                     onClick={() => setIsOpen(!isOpen)}
                     className="h-fit min-[675px]:hidden"
                 >
-                    {isOpen ? (
-                        <i className="ph ph-x text-[1.5rem]"></i>
-                    ) : (
-                        <i className="ph ph-list text-[1.5rem]"></i>
-                    )}
+                    {isOpen ? <XIcon size={24} /> : <ListIcon size={24} />}
                 </button>
             </nav>
             <div
                 id="mobile"
-                className={`${isOpen ? "flex" : "hidden"} animate-scale-in rounded-6 border-stroke bg-modal backdrop-blur-32 z-[99] m-12 flex-col items-center gap-32 border p-32`}
+                className={`${isOpen ? "flex" : "hidden"} animate-scale-in border-stroke bg-modal backdrop-blur-32 z-[99] flex-col items-center gap-32 rounded-[28px] border p-32`}
             >
                 <a href="/" className="text-h4 text-text">
                     Home
@@ -106,34 +102,39 @@ export default function Nav() {
                     About
                 </a>
                 <a
+                    href="/download"
+                    className="text-h4 text-text flex items-center gap-8"
+                >
+                    Download
+                    <ArrowLineDownIcon size={24} />
+                </a>
+                <a
                     href="https://github.com/OpenMarch/OpenMarch"
                     className="text-h4 text-text flex items-center gap-8"
                 >
-                    <LogoGitHub />
-                    Star
+                    GitHub
+                    <GithubLogoIcon size={24} />
                 </a>
                 <a
                     href="https://discord.gg/eTsQ98uZzq"
                     className="text-h4 text-text flex items-center gap-8"
                 >
-                    <LogoDiscord />
                     Discord
+                    <DiscordLogoIcon size={24} />
                 </a>
                 <a
                     href="https://www.patreon.com/openmarch"
                     className="text-h4 text-text flex items-center gap-8"
                 >
-                    <LogoPatreon />
-                    Donate
+                    Patreon
+                    <PatreonLogoIcon size={24} />
                 </a>
                 <a
                     href="https://store.openmarch.com"
-                    className="text-h4 text-text"
+                    className="text-h4 text-text flex items-center gap-8"
                 >
                     Merch
-                </a>
-                <a href="/download" className="text-h4 text-text">
-                    Download
+                    <TShirtIcon size={24} />
                 </a>
             </div>
         </nav>
