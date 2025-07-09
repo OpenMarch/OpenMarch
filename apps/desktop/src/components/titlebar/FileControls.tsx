7f22410576b9dfbf2e4eea07d1b6f5532645040a
dukc <dukkcc@proton.me>
2025-07-08 17:03:35 -0700

    only undo/redo in titlebar

--- apps/desktop/src/components/titlebar/FileControls.tsx
+++ apps/desktop/src/components/titlebar/FileControls.tsx
@@ -1,13 +1,5 @@
-import {
-    FileIcon,
-    FolderOpenIcon,
-    ArrowUUpLeftIcon,
-    ArrowUUpRightIcon,
-    FloppyDiskIcon,
-} from "@phosphor-icons/react";
+import { ArrowUUpLeftIcon, ArrowUUpRightIcon } from "@phosphor-icons/react";
 import * as api from "@/api/api";
-import { RegisteredActionsObjects } from "@/utilities/RegisteredActionsHandler";
-import RegisteredActionButton from "@/components/RegisteredActionButton";
 import { useFullscreenStore } from "@/stores/FullscreenStore";
 
 export default function FileControls() {
@@ -15,30 +7,6 @@ export default function FileControls() {
 
     return (
         <div className="titlebar-button flex w-fit gap-8">
-            <RegisteredActionButton
-                registeredAction={
-                    RegisteredActionsObjects.launchLoadFileDialogue
-                }
-                className="hover:text-accent focus-visible:text-accent outline-hidden duration-150 ease-out disabled:opacity-50"
-            >
-                <FolderOpenIcon size={18} />
-            </RegisteredActionButton>
-            <RegisteredActionButton
-                registeredAction={
-                    RegisteredActionsObjects.launchNewFileDialogue
-                }
-                className="hover:text-accent focus-visible:text-accent outline-hidden duration-150 ease-out disabled:opacity-50"
-            >
-                <FileIcon size={18} />
-            </RegisteredActionButton>
-            <RegisteredActionButton
-                registeredAction={
-                    RegisteredActionsObjects.launchSaveFileDialogue
-                }
-                className="hover:text-accent focus-visible:text-accent outline-hidden duration-150 ease-out disabled:opacity-50"
-            >
-                <FloppyDiskIcon size={18} />
-            </RegisteredActionButton>
             {!isFullscreen && (
                 <>
                     <button
