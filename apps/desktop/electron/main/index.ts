8be03bc5c38c12afb3d06fe2e1b1a58ebee2eade
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2025-07-10 22:10:19 -0400

    Audio import instructions Need to still do tempo import

--- apps/desktop/electron/main/index.ts
+++ apps/desktop/electron/main/index.ts
@@ -813,7 +813,10 @@ export async function insertAudioFile(): Promise<
     databaseResponse = await dialog
         .showOpenDialog(win, {
             filters: [
-                { name: "Audio File", extensions: ["mp3", "wav", "ogg"] },
+                {
+                    name: "Audio File (.mp3, .wav, .ogg)",
+                    extensions: ["mp3", "wav", "ogg"],
+                },
                 { name: "All Files", extensions: ["*"] },
             ],
         })
@@ -851,6 +854,7 @@ export async function insertAudioFile(): Promise<
                     },
                 };
 
+            win?.webContents.reload();
             // await setActiveDb(path.filePaths[0]);
             return databaseResponse;
         })
