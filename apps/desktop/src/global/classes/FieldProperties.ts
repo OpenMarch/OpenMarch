02286dc6bcfe3f338ddd2a2a2d9c8fc73a5c4713
Jake Pusateri <jakepusateri@gmail.com>
2025-07-12 08:44:07 -0400

    move field properties methods to the renderer

--- apps/desktop/src/global/classes/FieldProperties.ts
+++ apps/desktop/src/global/classes/FieldProperties.ts
@@ -1,4 +1,9 @@
 import { FieldTheme, DEFAULT_FIELD_THEME } from "./FieldTheme";
+import { db, schema } from "../database/db";
+import { eq } from "drizzle-orm";
+import { incrementUndoGroup } from "./History";
+
+const { field_properties } = schema;
 
 interface FieldPropertyArgs {
     name: string;
@@ -368,4 +373,47 @@ export interface SideDescriptions {
     terseLeft: string;
     /** E.g. "S2", "AR" or "SL" (short for "Side 2", "Audience Right" or "Stage Left") */
     terseRight: string;
+}
+
+export async function getFieldPropertiesJSON(): Promise<string> {
+    const fieldProperties = await db
+        .select()
+        .from(field_properties)
+        .where(eq(field_properties.id, 1))
+        .prepare()
+        .get();
+    if (!fieldProperties) {
+        throw new Error("Field properties not found");
+    }
+    return fieldProperties.json_data;
+}
+
+export async function getFieldProperties(): Promise<FieldProperties> {
+    const fieldPropertiesJson = await getFieldPropertiesJSON();
+    return new FieldProperties(JSON.parse(fieldPropertiesJson));
+}
+
+export async function updateFieldsPropertiesJSON(
+    fieldPropertiesJson: string,
+): Promise<string> {
+    const result = await db.transaction(async (tx) => {
+        await incrementUndoGroup(tx);
+
+        return await tx
+            .update(field_properties)
+            .set({ json_data: fieldPropertiesJson })
+            .where(eq(field_properties.id, 1))
+            .returning()
+            .get();
+    });
+    return result.json_data;
+}
+
+export async function updateFieldProperties(
+    fieldProperties: FieldProperties,
+): Promise<FieldProperties> {
+    const fieldPropertiesJson = JSON.stringify(fieldProperties);
+
+    const result = await updateFieldsPropertiesJSON(fieldPropertiesJson);
+    return new FieldProperties(JSON.parse(result));
 }
