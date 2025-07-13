4095f5968dce85519141329ebcb0c172ad1dbb7c
Dhruva Chakravarthi <70412345+dhruvachakravarthi@users.noreply.github.com>
2025-07-12 21:09:02 -0400

    Add new DS and update store

--- apps/desktop/src/stores/MarcherPageStore.ts
+++ apps/desktop/src/stores/MarcherPageStore.ts
@@ -1,18 +1,45 @@
+import {
+    MarcherPageMap,
+    MarcherPageNestedMap,
+} from "@/global/classes/MarcherPageIndex";
 import MarcherPage from "@/global/classes/MarcherPage";
 import { create } from "zustand";
 
 interface MarcherPageStoreInterface {
-    marcherPages: MarcherPage[];
+    marcherPages: MarcherPageMap;
+    marcherPagesAreLoading: boolean;
     fetchMarcherPages: () => Promise<void>;
 }
 
 export const useMarcherPageStore = create<MarcherPageStoreInterface>((set) => ({
-    marcherPages: [],
+    marcherPages: { marcherPagesByMarcher: {}, marcherPagesByPage: {} },
     marcherPagesAreLoading: true,
 
     fetchMarcherPages: async () => {
-        const newMarcherPages = await MarcherPage.getMarcherPages();
-        // Todo, create marcherPage objects only after we have optimized the getMarcherPages function
-        set({ marcherPages: newMarcherPages });
+        // Fetch all marcherPages from the DB
+        const rawMarcherPages = await MarcherPage.getMarcherPages();
+
+        // Create maps
+        const marcherPagesByMarcher: MarcherPageNestedMap = {};
+        const marcherPagesByPage: MarcherPageNestedMap = {};
+
+        // Populate maps
+        rawMarcherPages.forEach((mp) => {
+            if (!marcherPagesByMarcher[mp.marcher_id]) {
+                marcherPagesByMarcher[mp.marcher_id] = {};
+            }
+            marcherPagesByMarcher[mp.marcher_id][mp.page_id] = mp;
+
+            if (!marcherPagesByPage[mp.page_id]) {
+                marcherPagesByPage[mp.page_id] = {};
+            }
+            marcherPagesByPage[mp.page_id][mp.marcher_id] = mp;
+        });
+
+        // Update the store with the new maps
+        set({
+            marcherPages: { marcherPagesByMarcher, marcherPagesByPage },
+            marcherPagesAreLoading: false,
+        });
     },
 }));
