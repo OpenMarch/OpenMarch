3c63035fe5cc0a8446f0bdd75fae2fe74899fea2
Dhruva Chakravarthi <70412345+dhruvachakravarthi@users.noreply.github.com>
2025-07-12 21:12:09 -0400

    Revert "Add new DS and update store"
    This reverts commit 4095f5968dce85519141329ebcb0c172ad1dbb7c.

--- apps/desktop/src/stores/MarcherPageStore.ts
+++ apps/desktop/src/stores/MarcherPageStore.ts
@@ -1,45 +1,18 @@
-import {
-    MarcherPageMap,
-    MarcherPageNestedMap,
-} from "@/global/classes/MarcherPageIndex";
 import MarcherPage from "@/global/classes/MarcherPage";
 import { create } from "zustand";
 
 interface MarcherPageStoreInterface {
-    marcherPages: MarcherPageMap;
-    marcherPagesAreLoading: boolean;
+    marcherPages: MarcherPage[];
     fetchMarcherPages: () => Promise<void>;
 }
 
 export const useMarcherPageStore = create<MarcherPageStoreInterface>((set) => ({
-    marcherPages: { marcherPagesByMarcher: {}, marcherPagesByPage: {} },
+    marcherPages: [],
     marcherPagesAreLoading: true,
 
     fetchMarcherPages: async () => {
-        // Fetch all marcherPages from the DB
-        const rawMarcherPages = await MarcherPage.getMarcherPages();
-
-        // Create maps
-        const marcherPagesByMarcher: MarcherPageNestedMap = {};
-        const marcherPagesByPage: MarcherPageNestedMap = {};
-
-        // Populate maps
-        rawMarcherPages.forEach((mp) => {
-            if (!marcherPagesByMarcher[mp.marcher_id]) {
-                marcherPagesByMarcher[mp.marcher_id] = {};
-            }
-            marcherPagesByMarcher[mp.marcher_id][mp.page_id] = mp;
-
-            if (!marcherPagesByPage[mp.page_id]) {
-                marcherPagesByPage[mp.page_id] = {};
-            }
-            marcherPagesByPage[mp.page_id][mp.marcher_id] = mp;
-        });
-
-        // Update the store with the new maps
-        set({
-            marcherPages: { marcherPagesByMarcher, marcherPagesByPage },
-            marcherPagesAreLoading: false,
-        });
+        const newMarcherPages = await MarcherPage.getMarcherPages();
+        // Todo, create marcherPage objects only after we have optimized the getMarcherPages function
+        set({ marcherPages: newMarcherPages });
     },
 }));
