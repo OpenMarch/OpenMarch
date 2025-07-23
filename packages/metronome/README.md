71f19cb4fac225510b54bb7fe4555e41c42a3d84
Dhruva Chakravarthi <70412345+dhruvachakravarthi@users.noreply.github.com>
2025-07-23 14:14:05 -0400

    Add metronome package

---
+++ packages/metronome/README.md
@@ -0,0 +1,13 @@
+# Metronome
+
+A Metronome audio file generation utility for the OpenMarch project
+
+## Development
+
+1. Ensure you have [Node](https://nodejs.org/en/download) installed
+1. [pnpm](https://pnpm.io/installation) is optional but recommended
+1. Install dependencies
+
+   ```bash
+   pnpm install
+   ```
---
+++ packages/metronome/src/__test__/120 BPM Test/standard.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "vitest";
+
+import { type Measure, type Beat } from "../../metronome";
+
+const testName = "Standard 4/4 120 BPM Test";
+
+/**
+ * Empty Dummy test case.
+ */
+describe(testName, () => {
+    test(`Synthesize ${testName}`, async () => {
+        // Setup
+        // Create audio file from Measures
+        // Write the audio file
+    });
+});
+
+/**
+ * Replace the expected output with the actual expected output for your test case.
+ */
+const expected: Measure[] = [];
