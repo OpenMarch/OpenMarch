4545d5a480e9de285877f7e6e04633e412e43cca
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2025-08-24 09:48:39 -0400

    SQL.js test fixture for renderer
    Move fixtures to test root
    
    clean up exports
    
    remove absolute path

---
+++ apps/desktop/src/db-functions/__test__/connection.test.ts
@@ -0,0 +1,105 @@
+import { describe, expect } from "vitest";
+import { it, schema } from "@/test";
+import { eq } from "drizzle-orm";
+
+describe("Database connection", () => {
+    it("Database should be defined", async ({ db }) => {
+        expect(db).toBeDefined();
+    });
+
+    it("Database should be able to run a query", async ({ db }) => {
+        const result = await db.select().from(schema.marchers);
+        expect(result).toBeDefined();
+        expect(result.length).toBe(0);
+    });
+
+    it("Database should be able to create and delete a marcher", async ({
+        db,
+    }) => {
+        const preResult = await db.select().from(schema.marchers);
+        expect(preResult).toBeDefined();
+        expect(preResult.length).toBe(0);
+
+        const result = await db
+            .insert(schema.marchers)
+            .values({
+                section: "Test Section",
+                drill_prefix: "B",
+                drill_order: 1,
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
+            })
+            .returning({ id: schema.marchers.id });
+        expect(result).toBeDefined();
+        expect(result.length).toBe(1);
+
+        const postResult = await db.select().from(schema.marchers);
+        expect(postResult).toBeDefined();
+        expect(postResult.length).toBe(1);
+        expect(postResult[0].id).toBe(result[0].id);
+        expect(postResult[0].section).toBe("Test Section");
+        expect(postResult[0].drill_prefix).toBe("B");
+        expect(postResult[0].drill_order).toBe(1);
+
+        const deleteResult = await db
+            .delete(schema.marchers)
+            .where(eq(schema.marchers.id, result[0].id))
+            .returning({ id: schema.marchers.id });
+        expect(deleteResult).toBeDefined();
+        expect(deleteResult.length).toBe(1);
+        expect(deleteResult[0].id).toBe(result[0].id);
+
+        const postDeleteResult = await db.select().from(schema.marchers);
+        expect(postDeleteResult).toBeDefined();
+        expect(postDeleteResult.length).toBe(0);
+    });
+
+    it("Database should be able to update a marcher", async ({ db }) => {
+        const result = await db
+            .insert(schema.marchers)
+            .values({
+                section: "Test Section",
+                drill_prefix: "B",
+                drill_order: 1,
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
+            })
+            .returning({ id: schema.marchers.id });
+        expect(result).toBeDefined();
+        expect(result.length).toBe(1);
+        expect(result[0].id).toBeDefined();
+
+        const postResult = await db.select().from(schema.marchers);
+        expect(postResult).toBeDefined();
+        expect(postResult.length).toBe(1);
+        expect(postResult[0].id).toBe(result[0].id);
+        expect(postResult[0].section).toBe("Test Section");
+        expect(postResult[0].drill_prefix).toBe("B");
+        expect(postResult[0].drill_order).toBe(1);
+
+        const updateResult = await db
+            .update(schema.marchers)
+            .set({
+                section: "Updated Section",
+                drill_prefix: "C",
+                drill_order: 2,
+            })
+            .where(eq(schema.marchers.id, result[0].id))
+            .returning({
+                id: schema.marchers.id,
+                section: schema.marchers.section,
+            });
+        expect(updateResult).toBeDefined();
+        expect(updateResult.length).toBe(1);
+        expect(updateResult[0].id).toBe(result[0].id);
+        expect(updateResult[0].section).toBe("Updated Section");
+
+        const postUpdateResult = await db.select().from(schema.marchers);
+        expect(postUpdateResult).toBeDefined();
+        expect(postUpdateResult.length).toBe(1);
+        expect(postUpdateResult[0].id).toBe(result[0].id);
+        expect(postUpdateResult[0].section).toBe("Updated Section");
+        expect(postUpdateResult[0].drill_prefix).toBe("C");
+        expect(postUpdateResult[0].drill_order).toBe(2);
+    });
+});
---
+++ apps/desktop/src/db-functions/pathways.ts
@@ -0,0 +1,65 @@
+import { Path, Point } from "@openmarch/path-utility";
+import { DatabaseTransaction } from "./types";
+import { schema } from "@/global/database/db";
+import { assert } from "@/utilities/utils";
+import { eq } from "drizzle-orm";
+const { pathways } = schema;
+
+/**
+ * Updates the start or end point of a pathway.
+ * @param tx - The database transaction.
+ * @param pathwayId - The ID of the pathway to update.
+ * @param pathwayObj - The pathway object to update.
+ * @param newPoint - The new point to update the pathway to.
+ * @param type - The type of point to update, either "start" or "end".
+ */
+export async function updateEndpoint({
+    tx,
+    pathwayId,
+    newPoint,
+    type,
+}: {
+    tx: DatabaseTransaction;
+    pathwayId: number;
+    newPoint: Point;
+    type: "start" | "end";
+}) {
+    const pathwayResponse = await tx.query.pathways.findFirst({
+        where: eq(pathways.id, pathwayId),
+    });
+
+    if (!pathwayResponse) {
+        throw new Error("Pathway not found");
+    }
+
+    const pathwayObj = Path.fromJson(pathwayResponse.path_data);
+
+    type === "start"
+        ? pathwayObj.setStartPoint(newPoint)
+        : pathwayObj.setEndPoint(newPoint);
+
+    // assert that the point was updated
+    if (type === "start") {
+        assert(
+            pathwayObj.getStartPoint().x === newPoint.x &&
+                pathwayObj.getStartPoint().y === newPoint.y,
+            "Start point was not updated",
+        );
+    } else {
+        assert(
+            pathwayObj.getLastPoint().x === newPoint.x &&
+                pathwayObj.getLastPoint().y === newPoint.y,
+            "End point was not updated",
+        );
+    }
+
+    return tx
+        .update(pathways)
+        .set({
+            path_data: pathwayObj.toJson(),
+        })
+        .where(eq(pathways.id, pathwayId))
+        .returning({
+            id: pathways.id,
+        });
+}
