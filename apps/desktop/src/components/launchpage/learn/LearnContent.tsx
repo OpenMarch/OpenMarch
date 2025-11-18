f850e9bc8793bd746b81f82d1e3f8cd76b3b06c8
Jacob <97273940+dukcc@users.noreply.github.com>
2025-11-17 20:37:50 -0800

    add video guide link to learn page

--- apps/desktop/src/components/launchpage/learn/LearnContent.tsx
+++ apps/desktop/src/components/launchpage/learn/LearnContent.tsx
@@ -2,6 +2,7 @@ import * as Tabs from "@radix-ui/react-tabs";
 import Guides from "./Guides";
 import TroubleshootingTips from "./TroubleshootingTips";
 import { T } from "@tolgee/react";
+import { YoutubeLogoIcon } from "@phosphor-icons/react";
 
 export default function LearnContent() {
     return (
@@ -13,6 +14,29 @@ export default function LearnContent() {
                 <h3 className="text-h3">
                     <T keyName="launchpage.learn.title" />
                 </h3>
+                <div className="flex flex-col gap-8">
+                    <h4 className="text-h4">
+                        <T keyName="launchpage.learn.videoGuides.title" />
+                    </h4>
+                    <a
+                        href="https://www.youtube.com/playlist?list=PLQYV6VxuRWjz-1XwK9aDtEvHqdBJunZHn"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="bg-fg-1 border-stroke rounded-6 hover:border-accent flex flex-col overflow-clip border duration-150"
+                    >
+                        <div className="relative h-64">
+                            <div className="bg-accent absolute top-[-15%] right-[5%] -z-10 h-[3rem] w-[7rem] rounded-full text-transparent opacity-75 blur-2xl">
+                                image placeholder
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-6 p-8">
+                            <YoutubeLogoIcon size={24} />
+                            <p className="text-body">
+                                <T keyName="launchpage.learn.videoGuides.description" />
+                            </p>
+                        </div>
+                    </a>
+                </div>
                 <Guides />
                 <TroubleshootingTips />
             </div>
