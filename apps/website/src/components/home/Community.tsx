c979b83fd3aade881f90ecfcc01820b97a99d59c
Jacob <97273940+dukcc@users.noreply.github.com>
2025-11-24 22:01:28 -0800

    fix some image qualities

--- apps/website/src/components/home/Community.tsx
+++ apps/website/src/components/home/Community.tsx
@@ -109,8 +109,6 @@ export default function Community() {
                 >
                     <Button variant="primary">Fork the repo</Button>
                 </a>
-
-                <div className="bg-accent -top-1/5 left-1/2 -z-50 size-[250px] -translate-x-1/2 rounded-full opacity-40 blur-[9vmin] max-[850px]:top-[70%] max-[850px]:size-[150px] max-[850px]:blur-[15vmin]"></div>
             </div>
 
             <div className="bg-fg-1 border-stroke rounded-6 flex flex-col gap-12 border p-12">
