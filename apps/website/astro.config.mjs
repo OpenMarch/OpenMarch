1a228a558712de224afcab3b582f7feee63471fc
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2025-11-25 00:44:04 -0500

    use wrangler

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -9,71 +9,76 @@ import starlightImageZoom from "starlight-image-zoom";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    redirects: {
-        "/alexdumo": "/about/alexdumo",
-        "/developers/contributing": "/developers/codebase",
-        "/about/submitting-feedback/": "/guides/submitting-feedback/",
-    },
-    integrations: [
-        react(),
-        starlight({
-            plugins: [starlightImageZoom()],
-            title: "OpenMarch",
-            logo: {
-                light: "./public/openmarch-black.svg",
-                dark: "./public/openmarch-white.svg",
-                replacesTitle: true,
-            },
-            social: {
-                discord: "https://discord.gg/eTsQ98uZzq",
-                github: "https://github.com/OpenMarch/OpenMarch",
-                patreon: "https://www.patreon.com/c/openmarch",
-                youtube: "https://www.youtube.com/@OpenMarchApp",
-            },
-            editLink: {
-                baseUrl:
-                    "https://github.com/OpenMarch/OpenMarch/tree/main/apps/website/",
-            },
-            disable404Route: true,
-            favicon: "./public/favicon.png",
-            customCss: [
-                "@fontsource/dm-sans/400.css",
-                "@fontsource/dm-sans/600.css",
-                "@fontsource/dm-mono/400.css",
-                "./src/styles/starlight.css",
-            ],
-            sidebar: [
-                {
-                    label: "Guides",
-                    autogenerate: { directory: "guides" },
-                },
-                {
-                    label: "About",
-                    autogenerate: { directory: "about" },
-                },
-                {
-                    label: "Developers",
-                    autogenerate: { directory: "developers" },
-                },
-            ],
-        }),
-        mdx(),
-        sitemap(),
-        yeskunallumami({
-            id: "09a72d11-c8dd-43b0-9ee8-e2c7c492bc21",
-            endpointUrl: "https://umami.cool.dukc.dev",
-        }),
-    ],
+  redirects: {
+      "/alexdumo": "/about/alexdumo",
+      "/developers/contributing": "/developers/codebase",
+      "/about/submitting-feedback/": "/guides/submitting-feedback/",
+  },
 
-    site: "https://openmarch.com",
+  integrations: [
+      react(),
+      starlight({
+          plugins: [starlightImageZoom()],
+          title: "OpenMarch",
+          logo: {
+              light: "./public/openmarch-black.svg",
+              dark: "./public/openmarch-white.svg",
+              replacesTitle: true,
+          },
+          social: {
+              discord: "https://discord.gg/eTsQ98uZzq",
+              github: "https://github.com/OpenMarch/OpenMarch",
+              patreon: "https://www.patreon.com/c/openmarch",
+              youtube: "https://www.youtube.com/@OpenMarchApp",
+          },
+          editLink: {
+              baseUrl:
+                  "https://github.com/OpenMarch/OpenMarch/tree/main/apps/website/",
+          },
+          disable404Route: true,
+          favicon: "./public/favicon.png",
+          customCss: [
+              "@fontsource/dm-sans/400.css",
+              "@fontsource/dm-sans/600.css",
+              "@fontsource/dm-mono/400.css",
+              "./src/styles/starlight.css",
+          ],
+          sidebar: [
+              {
+                  label: "Guides",
+                  autogenerate: { directory: "guides" },
+              },
+              {
+                  label: "About",
+                  autogenerate: { directory: "about" },
+              },
+              {
+                  label: "Developers",
+                  autogenerate: { directory: "developers" },
+              },
+          ],
+      }),
+      mdx(),
+      sitemap(),
+      yeskunallumami({
+          id: "09a72d11-c8dd-43b0-9ee8-e2c7c492bc21",
+          endpointUrl: "https://umami.cool.dukc.dev",
+      }),
+  ],
 
-    devToolbar: {
-        enabled: false,
-    },
+  site: "https://openmarch.com",
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  devToolbar: {
+      enabled: false,
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
 });
