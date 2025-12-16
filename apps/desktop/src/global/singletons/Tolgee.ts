7fa587a4d24cf2b4488a396d45e649c0474bb5cc
John Bostick <john.bostick@gatech.edu>
2025-12-16 11:38:35 -0500

    Make loading dev tools synchronous

--- apps/desktop/src/global/singletons/Tolgee.ts
+++ apps/desktop/src/global/singletons/Tolgee.ts
@@ -2,37 +2,27 @@ import { Tolgee, DevTools, FormatSimple } from "@tolgee/react";
 import { FormatIcu } from "@tolgee/format-icu";
 
 const TOLGEE_API_URL = "https://app.tolgee.io";
+const tolgeeDevTools = !!window.electron?.getSettingSync?.("tolgeeDevTools");
+const tolgeeApiKey = window.electron?.getSettingSync?.("tolgeeApiKey");
 
-async function initTolgee() {
-    const tolgeeDevTools = await window.electron.invoke(
-        "settings:get",
-        "tolgeeDevTools",
-    );
-    const tolgeeApiKey = await window.electron.invoke(
-        "settings:get",
-        "tolgeeApiKey",
-    );
+const tolgee = Tolgee()
+    .use(DevTools())
+    .use(FormatSimple())
+    .use(FormatIcu())
+    .init({
+        language: "en",
+        ...(tolgeeDevTools &&
+            tolgeeApiKey && {
+                apiUrl: TOLGEE_API_URL,
+                apiKey: tolgeeApiKey,
+            }),
+        staticData: {
+            en: () => import("../../../i18n/en.json"),
+            es: () => import("../../../i18n/es.json"),
+            fr: () => import("../../../i18n/fr.json"),
+            "pt-BR": () => import("../../../i18n/pt-BR.json"),
+            ja: () => import("../../../i18n/ja.json"),
+        },
+    });
 
-    return Tolgee()
-        .use(DevTools())
-        .use(FormatSimple())
-        .use(FormatIcu())
-        .init({
-            language: "en",
-            ...(tolgeeDevTools &&
-                tolgeeApiKey && {
-                    apiUrl: TOLGEE_API_URL,
-                    apiKey: tolgeeApiKey,
-                }),
-            staticData: {
-                en: () => import("../../../i18n/en.json"),
-                es: () => import("../../../i18n/es.json"),
-                fr: () => import("../../../i18n/fr.json"),
-                "pt-BR": () => import("../../../i18n/pt-BR.json"),
-                ja: () => import("../../../i18n/ja.json"),
-            },
-        });
-}
-
-const tolgee = await initTolgee();
 export default tolgee;
