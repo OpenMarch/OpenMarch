d864c8cba01039796bc9d167d9f0020a80a020cd
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2025-12-24 16:29:46 -0500

    wrap text on for recent files on launch page

--- packages/ui/src/components/base/ListItem.tsx
+++ packages/ui/src/components/base/ListItem.tsx
@@ -15,7 +15,7 @@ export const ListItem = ({
     return (
         <div
             className={clsx(
-                `rounded-6 flex w-full items-center justify-start gap-8 ${selected && "border-stroke bg-fg-2 border"} text-text h-[2.5rem] px-12`,
+                `rounded-6 flex w-full items-center justify-start gap-8 ${selected && "border-stroke bg-fg-2 border"} text-text min-h-[2.5rem] min-w-0 px-12`,
                 className,
             )}
         >
