6b94c8e4e298f39895165f6cfbc8abe09e6e5bdf
user.email <alex.dumouchelle484@gmail.com>
2026-07-27 09:43:34 -0500

    Calculation function

---
+++ packages/core/src/coordinates/types.ts
@@ -0,0 +1,18 @@
+/** Rendering timeline for a single marcher across the entire show. */
+export type MarcherTimeline = {
+    /**
+     * Ordered list of timestamps, in milliseconds, for each coordinate.
+     */
+    timestamps: Float32Array;
+    /**
+     * Ordered list of coordinates, in the same order as the timestamps.
+     *
+     * `coordinates.length === timestamps.length * 2`
+     */
+    coordinates: Float32Array;
+    // /**
+    //  * Ordered list of pathways
+    //  */
+    // pathways: Float32Array;
+    // pathwayIndexes: Float32Array;
+};
