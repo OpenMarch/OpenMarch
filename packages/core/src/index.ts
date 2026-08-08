4b701294d08b62fb6bb2156bd2adc3b8f56564c5
user.email <alex.dumouchelle484@gmail.com>
2026-08-08 09:23:14 -0400

    queries for marcher timelines

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,7 @@
-// Main entry point for @openmarch/core
-// Export all core features
-
 // Field-related exports
 export * from "./field";
 export * from "./shapes";
 export * from "./path-utility";
 
-// Future core features can be added here:
-// export * from "./marching";
-// export * from "./music";
-// export * from "./utilities";
+// Coordinates-related exports
+export * from "./coordinates";
