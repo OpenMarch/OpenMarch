cd51a925fad27ce959c3bb1eadeda3600e6da72b
user.email <alex.dumouchelle484@gmail.com>
2026-08-08 14:12:17 -0400

    invalidate timelines on edit

--- apps/desktop/src/hooks/queries/sharedInvalidators.ts
+++ apps/desktop/src/hooks/queries/sharedInvalidators.ts
@@ -4,6 +4,8 @@ import { QueryClient } from "@tanstack/react-query";
 import { shapePageKeys } from "./useShapePages";
 import { pageKeys } from "./usePages";
 import { marcherAppearancesKeys } from "./useMarcherAppearances";
+import { marcherTimelineKeys } from "./useMarcherTimelines";
+import { useFrameClockStore } from "@/services/clock/frame-clock";
 /**
  * Invalidate the marcher pages and coordinate data queries for a given page id
  *
@@ -33,5 +35,32 @@ export const invalidateByPage = (qc: QueryClient, pageIds: Set<number>) => {
                     queryKey: marcherAppearancesKeys.byPageId(pageId),
                 });
             });
+    }
+};
+
+export const invalidateAllMarchers = (qc: QueryClient) => {
+    const promises = [
+        qc.invalidateQueries({
+            queryKey: marcherTimelineKeys.all(),
+        }),
+    ];
+
+    // Wait for all promises to resolve, then increment the clock version
+    // This is to trigger a re-render for all subscribers of the frame clock
+    void Promise.all(promises).then(() => {
+        useFrameClockStore.setState(({ _version }) => ({
+            _version: _version + 1,
+        }));
+    });
+};
+
+export const invalidateByMarchers = (
+    qc: QueryClient,
+    marcherIds: Set<number>,
+) => {
+    for (const marcherId of marcherIds) {
+        void qc.invalidateQueries({
+            queryKey: marcherTimelineKeys.byMarcherId(marcherId),
+        });
     }
 };
