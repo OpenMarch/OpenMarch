eadf762508c7a002b896dbc35206fb88d5313eea
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2026-09-08 21:54:22 -0400

    fix: use ASCII hyphen in measure ranges for PDF fonts (#1025)
    * Switch '->' icon to '-' since it cannot be included in font
    
    * fix tests

--- apps/desktop/src/global/classes/Page.utils.ts
+++ apps/desktop/src/global/classes/Page.utils.ts
@@ -42,7 +42,7 @@ export const measureRangeString = (
                 : `${lastMeasure.number}(${beatToEndOn})`;
 
         if (firstMeasureString === lastMeasureString) return firstMeasureString;
-        return `${firstMeasureString} → ${lastMeasureString}`;
+        return `${firstMeasureString} - ${lastMeasureString}`;
     } catch (err) {
         return "N/A";
     }
